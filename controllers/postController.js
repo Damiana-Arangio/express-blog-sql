d633866995fc87caa5314357f9d6b10a42785eb5
Damiana-Arangio <arangio.damiana@gmail.com>
2025-10-29 20:39:21 +0100

    Implementazione Milestone 2 completata: - Completata logica funzione Index per recuperare lista dei post dal database - Verificato il corretto funzionamento su Postman

--- controllers/postController.js
+++ controllers/postController.js
@@ -8,25 +8,52 @@ const connection = require('../data/db.js')    // Import della connessione al Da
     FUNZIONI ROTTE
 *********************/
 
-// INDEX - mostra tutti i post
+// ------------------------------------------------------------------ ROTTA INDEX ----------------------------------------------------------------------------
+
+/* INDEX - mostra tutti i post */
 function index (req, res) {
-    res.send("Lista dei post");
+
+    const query_sql = 'SELECT * FROM posts';        // Definizione della query SQL 
+
+    connection.query(query_sql, (err, results) => { // Esecuzione query SQL
+
+        // Gestione in caso di fallimento - codice di stato HTTP 500
+        if (err) { 
+            return res.status(500).json({
+                error: "Errore durante il recupero dei post dal database"
+            });
+        }
+        // Gestione in caso di successo - lista dei post 
+        res.json(results); 
+    })
 }
+
+
+// ------------------------------------------------------------------ ROTTA SHOW ----------------------------------------------------------------------------
 
 // SHOW - mostra un post specifico
 function show(req, res) {
     res.send("Dettagli del post: " + req.params.id);
 }
 
+
+// ------------------------------------------------------------------ ROTTA STORE ----------------------------------------------------------------------------
+
 // STORE - crea un nuovo post
 function store(req, res) {
     res.send("Creato nuovo post");
 }
 
+
+// ------------------------------------------------------------------ ROTTA UPDATE ----------------------------------------------------------------------------
+
 // UPDATE - Aggiorna un post
 function update(req, res) {
     res.send("Aggiornato post: " + req.params.id );
 }
+
+
+// ------------------------------------------------------------------ ROTTA DESTROY  ----------------------------------------------------------------------------
 
 // DESTROY - Elimina un post
 function destroy(req, res) {
